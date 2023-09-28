81ddbec1853b6e4cfa52c394f728f5a62a446576
Nk <113770164+Njuezgotswish@users.noreply.github.com>
2023-09-28 15:19:51 +0300

    Update 0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
-"""
-Island Perimeter
-"""
+
+""" Function to find perimiter of an island """
+
 
 def island_perimeter(grid):
     """
-     returns the perimeter of the island described in grid
-    :param grid:
-    :return:
+    Input: List of Lists
+    Returns: Perimeter of the island
     """
-    area = 0
-    for row in grid + list(map(list, zip(*grid))):
-        for i1, i2 in zip([0] + row, row + [0]):
-            area += int(i1 != i2)
-    return area
+    count = 0
+    row = len(grid)
+    col = len(grid[0]) if row else 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+
+            idx = [(i - 1, j), (i, j - 1), (i, j + 1), (i + 1, j)]
+            check = [1 if k[0] in range(row) and k[1] in range(col) else 0
+                     for k in idx]
+
+            if grid[i][j]:
+                count += sum([1 if not r or not grid[k[0]][k[1]] else 0
+                              for r, k in zip(check, idx)])
+
+    return (count)
